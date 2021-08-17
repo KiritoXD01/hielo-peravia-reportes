05b7953f533c7ec83989c52c482c19d3bf059161
Javier Mercedes <manuelmercedez10@gmail.com>
2021-08-16 21:50:35 -0400

    Added form, logo and email js library

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <img src="./assets/logo.jpeg" alt="logo" />
+        <form autocomplete="off">
+            <label>Nombre del Negocio</label>
+            <input
+                type="text"
+                v-model="nombre_negocio"
+                placeholder="Nombre del Negocio..."
+                maxlength="255"
+                required
+            />
+            <label>Direcci&oacute;n</label>
+            <input
+                type="text"
+                v-model="direccion"
+                placeholder="Dirección..."
+                maxlength="500"
+                required
+            />
+            <label>Tel&eacute;fono</label>
+            <input
+                type="tel"
+                v-model="telefono"
+                placeholder="Teléfono..."
+                maxlength="10"
+                required
+            />
+            <label>Celular</label>
+            <input
+                type="tel"
+                v-model="celular"
+                placeholder="Celular..."
+                maxlength="10"
+            />
+            <label>Descripción</label>
+            <input
+                type="text"
+                v-model="descripcion"
+                placeholder="Descripción..."
+                maxlength="500"
+            />
+            <button type="submit">Enviar Ticket de Reporte</button>
+        </form>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            nombre_negocio: "",
+            direccion: "",
+            telefono: "",
+            celular: "",
+            descripcion: ""
+        };
+    }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+* {
+    box-sizing: border-box;
+}
+
+.container {
+    display: block;
+    margin: auto;
+    text-align: center;
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+    width: 50%;
+}
+
+label {
+    float: left;
+}
+
+input[type="text"],
+[type="tel"] {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-top: 6px;
+    margin-bottom: 16px;
+}
+
+button[type="submit"] {
+    background-color: #4caf50;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+button[type="submit"]:hover {
+    background-color: #45a049;
+}
+
+img {
+    width: 100%;
+    padding-bottom: 15px;
 }
 </style>
